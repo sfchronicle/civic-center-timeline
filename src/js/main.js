f477d573cd2a033094da605a67505fd08c097ef4
emma <emmaruthoneill@gmail.com>
2017-02-09 09:20:41 -0800

    initial commit

---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+require("./lib/social"); //Do not delete
+
+var mySwiper = new Swiper ('.swiper-container', {
+  direction: 'horizontal',
+  loop: false,
+  grabCursor: true,
+  pagination: '.swiper-pagination',
+  keyboardControl: true,
+  mousewheelControl: true,
+  nextButton: '.swiper-button-next',
+  prevButton: '.swiper-button-prev',
+  pagination: '.swiper-pagination',
+  paginationType: 'progress'
+})  
