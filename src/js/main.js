45c1532f97fb4e4afc146e66f0fdf41de456e810
emma <emmaruthoneill@gmail.com>
2017-02-10 08:28:17 -0800

    fixing iphone bugs

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,1 @@
 require("./lib/social"); //Do not delete
-
-var mySwiper = new Swiper ('.swiper-container', {
-  direction: 'horizontal',
-  loop: false,
-  grabCursor: true,
-  pagination: '.swiper-pagination',
-  keyboardControl: true,
-  mousewheelControl: true,
-  nextButton: '.swiper-button-next',
-  prevButton: '.swiper-button-prev',
-  pagination: '.swiper-pagination',
-  paginationType: 'progress'
-})  
